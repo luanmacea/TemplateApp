139873228f81fc75397b42c526cbd620ccb0108f
luanmacea <luanmacea@gmail.com>
2024-01-08 13:11:53 -0300

    configuracoes iniciais

---
+++ src/routes/drawer.routes.js
@@ -0,0 +1,22 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Feather } from "@expo/vector-icons";
+
+import Home from "../screen/Home";
+
+const Drawer = createDrawerNavigator();
+
+export default function DrawerRoutes() {
+  return (
+    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          drawerIcon: ({ color }) => (
+            <Feather name="home" size={24} color={color} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+// import TabRoutes from "./tab.routes";
+import DrawerRoutes from './drawer.routes'
+// import StackRoutes from "./stack.routes";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <DrawerRoutes />
+    </NavigationContainer>
+  );
+}
